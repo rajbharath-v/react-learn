634b2f4b98118cbfeb7657f0bffa6931f365f794
rajbharath-v <rajbharath5858@gmail.com>
2025-08-01 14:55:33 +0530

    new file add

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import React from 'react' ;
 import Hello from './Component/Hello';
 import './Style/app.css';
+import Welcome from './Component/Welcome';
 
 function App({note}){
 
@@ -12,15 +13,14 @@ function App({note}){
     <div>
         <h2>Hello welcome to react</h2>
         <p>hello </p>
-
         <ul>
           {
             note.map((item)=><li key={item.id}>{item.name}</li>)
           }
         </ul>
         <p>{`myname is ${name}`}</p>
-        <Hello/>
         <button className='bg-danger p-2'>submit</button>
+        <Welcome/>
     </div>
   )
 }
---
+++ src/Component/Welcome.jsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+function Welcome(){
+    return(
+        <div>
+            <p>hello world</p>
+        </div>
+    )
+}
+
+export default Welcome;
