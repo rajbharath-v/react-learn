9d2c16e6edd5e1a525465571befd6ebce99e507a
rajbharath-v <rajbharath5858@gmail.com>
2025-08-02 13:20:48 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,16 @@
+import React, { useEffect } from 'react' ;
 
+function App(){
 
-import React, { useState } from 'react' ;
-import Hello from './Component/Hello.jsx' ;
-import Welcome from './Component/Welcome.jsx' ;
-import Notes from './Component/Notes.jsx' ;
-function App({note}){
-
-  let [details, setDetails ] =useState({
-    name:"" ,
-    password:"" 
-  }) ;
-
-  let [count,setCount] = useState(0);
-
-  let [newDetails, setNewDetails] = useState([]);
-  console.log(newDetails);
-  console.log('name' ,details);
-  let handleDetails =(e)=>{
-    e.preventDefault() ;
-
-    let newDetail = {
-      name:details.name,
-      password:details.password
-    }
-    setNewDetails(newDetail);
-  }
-
-  let [statement, setStatement] = useState(true);
-
+  useEffect(()=>{
+    document.title = "React hooks"
+  })
 
   return(
     <div>
-      {
-        statement ? 
-        ("statement True") :("statement false") 
-      }
-        <p>count : {count}</p>
-        <Hello note={note}/>
-        <Welcome/>
-        <Notes/>
-        <form onSubmit={handleDetails}>
-          <input
-          type='text'
-          placeholder='Enter your name'
-          onChange={(event)=>setDetails({
-            ...details,
-            name:event.target.value
-          })}
-          value = {details.name}
-        />
-        <br/>
-        <input
-          type='password'
-           placeholder='Enter your password'
-          onChange={(event)=>setDetails({
-            ...details,
-            password:event.target.value
-          })}
-          value = {details.password}
-        />
-          <button type='submit'>submit</button>
-        </form>
-        <div>
-          <button onClick={()=>setCount(count + 1)}>Increment</button>
-          <button onClick={()=>setCount(count - 1) }>Decrement</button>
-          <button onClick={()=>setCount(0)}>Reset</button>
-        </div>
+        <p>Hooks concepts</p>
     </div>
   )
 }
 
-export default App ;
+export default App;
